accbd501874caa03071aeb2bad28ca63b23c1945
세순 송 <sss010201sss@naver.com>
2023-03-24 15:30:42 +0900

    2023.03.22.3.33

---
+++ UsingOut/MainApp.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace UsingOut
+{
+    class MainApp
+    {
+        static void Divide(int a, int b, out int quotient, out int remainder)
+        {
+            quotient = a / b;
+            remainder = a % b;
+        }
+
+        static void (string[] args)
+        {
+            int a = 20;
+            int b = 3;
+            //int c;
+            //int b;
+
+            Divide(a, b, out int c, out int d);
+
+            Console.WriteLine($"a:{a}, b:{b}, a/b:{c}, a%b:{d}");
+            }
+       }
