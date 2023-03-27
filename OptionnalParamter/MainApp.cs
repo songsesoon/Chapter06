0f60ee1753c8dcd7abe2899b85ba687d6244bbe8
세순 송 <sss010201sss@naver.com>
2023-03-27 14:59:24 +0900

    20023.03.24

---
+++ OptionnalParamter/MainApp.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace OptionnalParamter
+{
+    class MainApp
+    {
+        static void PrintProfile(string name, string phone = "")
+        {
+            Console.WriteLine($"Name: {name}, Phone: {phone}");
+        }
+
+        static void Main(string[] args)
+        {
+            PrintProfile("중근");
+            PrintProfile("관순", "010-123-1234");
+            PrintProfile("봉길");
+            PrintProfile("동주", phone: "010-789=7890");
+        }
+    }
+}
